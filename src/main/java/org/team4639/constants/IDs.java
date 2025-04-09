a7914ff6c6f2807344b7a0e58ea4f9bb499c67bf
aditya-khambekar <adityakhambekar9@gmail.com>
2025-04-08 20:12:50 -0500

    add: real motor IDs

--- src/main/java/org/team4639/constants/IDs.java
+++ src/main/java/org/team4639/constants/IDs.java
@@ -2,8 +2,8 @@ package org.team4639.constants;
 
 // TODO: get IDS
 public class IDs {
-  public static final int ELEVATOR_LEFT = 0;
-  public static final int ELEVATOR_RIGHT = 1;
+  public static final int ELEVATOR_LEFT = 21;
+  public static final int ELEVATOR_RIGHT = 22;
 
-  public static final int SCORING_MOTOR = 2;
+  public static final int SCORING_MOTOR = 23;
 }
