1eef6bdeb4edb47db6aad4f4d453689444b02dbf
aditya-khambekar <adityakhambekar9@gmail.com>
2025-04-09 09:08:50 -0500

    fix: removed SYSID mode because it lives in auton

--- src/main/java/org/team4639/_robot/RobotMode.java
+++ src/main/java/org/team4639/_robot/RobotMode.java
@@ -1,21 +1,16 @@
 package org.team4639._robot;
 
 public class RobotMode {
-  private static byte RobotMode = 0b00;
+  private static byte RobotMode = 0b0;
   // 0 = COMP
-  // 1 = SYSID
-  // 2 = MANUAL
+  // 1 = MANUAL
 
   public static boolean isComp() {
-    return RobotMode == 0b00;
-  }
-
-  public static boolean isSysID() {
-    return RobotMode == 0b01;
+    return RobotMode == 0b0;
   }
 
   public static boolean isManual() {
-    return RobotMode == 0b10;
+    return RobotMode == 0b1;
   }
 
   public static void setRobotMode(byte robotMode) {
