452f96c8baf70197115eeb3f4dc5c073e89e7236
aditya-khambekar <adityakhambekar9@gmail.com>
2025-04-24 10:11:45 -0500

    add: AutoChooser singleton

---
+++ src/main/java/org/team4639/auto/AutoChooser.java
@@ -0,0 +1,18 @@
+package org.team4639.auto;
+
+import java.util.Objects;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class AutoChooser extends SendableChooser<Command>{
+    private static volatile AutoChooser instance;
+    
+    public static synchronized AutoChooser getInstance() {
+        return instance = Objects.requireNonNullElseGet(instance, AutoChooser::new);
+    }
+
+    private AutoChooser() {
+        //add autos here once compiled
+    }    
+}
