1be7a686d98dc0563678c3d67e475e45af38178e
Aditya <adityakhambekar9@gmail.com>
2025-09-27 22:20:44 -0500

    add URCL vendordep

--- src/main/java/org/team4639/BuildConstants.java
+++ src/main/java/org/team4639/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "HOTS-Rewritten2";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 143;
-  public static final String GIT_SHA = "0f9bad40a41c69622a78bf849faca13e06e7fbcf";
-  public static final String GIT_DATE = "2025-09-27 11:12:33 EDT";
-  public static final String GIT_BRANCH = "rollback";
-  public static final String BUILD_DATE = "2025-09-27 23:03:40 EDT";
-  public static final long BUILD_UNIX_TIME = 1759028620876L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 144;
+  public static final String GIT_SHA = "61ab53eef54560bc374f1eb9f10efb4c899dc323";
+  public static final String GIT_DATE = "2025-09-27 23:07:38 EDT";
+  public static final String GIT_BRANCH = "master";
+  public static final String BUILD_DATE = "2025-09-27 23:20:38 EDT";
+  public static final long BUILD_UNIX_TIME = 1759029638902L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
