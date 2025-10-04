bab3262742da853fdf0a3cae18f7158a13ee175b
Aditya <adityakhambekar9@gmail.com>
2025-10-04 13:20:52 -0500

    added REVLib PID to wrist

--- src/main/java/org/team4639/BuildConstants.java
+++ src/main/java/org/team4639/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "HOTS-Rewritten2";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 148;
-  public static final String GIT_SHA = "2ce3b6292edb20dd6177ff94b75474efeafa0dd5";
-  public static final String GIT_DATE = "2025-10-03 16:01:52 EDT";
+  public static final int GIT_REVISION = 150;
+  public static final String GIT_SHA = "a71a05d2b69eeefca237e1698de650002278b769";
+  public static final String GIT_DATE = "2025-10-04 11:19:04 EDT";
   public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "2025-10-04 07:49:51 EDT";
-  public static final long BUILD_UNIX_TIME = 1759578591715L;
+  public static final String BUILD_DATE = "2025-10-04 12:11:16 EDT";
+  public static final long BUILD_UNIX_TIME = 1759594276870L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
---
+++ src/main/java/org/team4639/robot/subsystems/superstructure/subsystems/wrist/io/WristIOREVPID.java
@@ -0,0 +1,79 @@
+package org.team4639.robot.subsystems.superstructure.subsystems.wrist.io;
+
+import static edu.wpi.first.units.Units.*;
+
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel;
+import com.revrobotics.spark.config.AbsoluteEncoderConfig;
+import com.revrobotics.spark.config.ClosedLoopConfig;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Dimensionless;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.team4639.robot.robot.Subsystems;
+
+public class WristIOREVPID extends WristIO {
+    SparkFlex sparkFlex;
+
+    ArmFeedforward withoutCoral;
+    ArmFeedforward withCoral;
+
+    private double lastRequestedVolts = 0;
+
+    public WristIOREVPID(int ID) {
+        sparkFlex = new SparkFlex(ID, SparkLowLevel.MotorType.kBrushless);
+
+        var PIDConfig = new SparkFlexConfig().closedLoop;
+        PIDConfig
+                .feedbackSensor(ClosedLoopConfig.FeedbackSensor.kPrimaryEncoder)
+                .p(48.0 / 12)
+                .outputRange(-1, 1)
+                .maxMotion
+                .maxAcceleration(180)
+                .maxVelocity(60);
+
+        sparkFlex.configure(
+                new SparkFlexConfig().apply(new AbsoluteEncoderConfig().zeroOffset(0.46)).apply(PIDConfig),
+                ResetMode.kNoResetSafeParameters,
+                PersistMode.kPersistParameters);
+
+        withoutCoral = new ArmFeedforward(0, 0.22, 0);
+        withCoral = new ArmFeedforward(0, 0.28, 0);
+    }
+
+    @Override
+    public void updateInputs(WristIOInputs inputs) {
+        inputs.motorCurrent.mut_replace(Amps.of(sparkFlex.getOutputCurrent()));
+        inputs.motorPosition.mut_replace(Rotations.of(sparkFlex.getAbsoluteEncoder().getPosition()));
+        inputs.motorTemperature.mut_replace(Celsius.of(sparkFlex.getMotorTemperature()));
+        inputs.motorVelocity.mut_replace(
+                RotationsPerSecond.of(sparkFlex.getAbsoluteEncoder().getVelocity() / 60.));
+        inputs.busVoltage.mut_replace(Volts.of(sparkFlex.getBusVoltage()));
+        inputs.requestedVoltage.mut_replace(Volts.of(lastRequestedVolts));
+    }
+
+    @Override
+    public void setDutyCycleOutput(Dimensionless percent) {
+        sparkFlex.set(percent.in(Value));
+        lastRequestedVolts = percent.in(Value) * 12;
+    }
+
+    @Override
+    public void setPosition(Angle position) {
+        sparkFlex.getClosedLoopController().setReference(position.in(Rotations), SparkBase.ControlType.kMAXMotionPositionControl);
+    }
+
+    @Override
+    public void setVoltage(Voltage voltage) {
+        sparkFlex.setVoltage(voltage);
+        lastRequestedVolts = voltage.in(Volts);
+    }
+}
